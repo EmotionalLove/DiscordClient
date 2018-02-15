468730222979637e67bb5d54366724e31b213489
Sasha <totsuka.sama@gmail.com>
2018-02-15 12:16:59 -0800

    css style for field

--- src/com/discclient/Main.java
+++ src/com/discclient/Main.java
@@ -1,16 +1,12 @@
 package com.discclient;
 
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -19,6 +15,7 @@ public class Main extends Application {
 
     double w = 325;
     double h = 275;
+    static String orignalProperty;
 
     @Override
     public void start(Stage primaryStage) {
@@ -53,20 +50,34 @@ public class Main extends Application {
         label1.setLayoutX(w/2 - label1.getLayoutBounds().getWidth()/2);
         label1.setLayoutY(h/2);
         Button button = new Button("Login");
+        button.setDisable(true);
         button.setLayoutY(h - 30);
         TextField field = new TextField();
         field.setPrefWidth(200);
+        orignalProperty = field.getStyle();
         field.setPromptText("Token");
-        field.setOnKeyTyped(e -> {
+        field.textProperty().addListener(e -> {
             if (field.getText().length() == 0) {
                 button.setDisable(true);
+                field.setStyle(orignalProperty);
                 return;
             }
+            field.setStyle(orignalProperty);
             button.setDisable(false);
         });
         field.setLayoutX(w/2 - field.getPrefWidth()/2);
         field.setLayoutY(h - 30);
         button.setLayoutX(field.getLayoutX() + field.getPrefWidth() + 10);
+
+
+        button.setOnMouseClicked(e -> {
+            try {
+                DiscordHandler.connect(field.getText());
+            }catch (Exception ex) {
+                field.setStyle("-fx-faint-focus-color: transparent; -fx-focus-color:rgba(255,0,0,0.7);");
+                field.requestFocus();
+            }
+        });
 
         s.widthProperty().addListener((observable, oldValue, newValue) -> {
             w = newValue.doubleValue();
